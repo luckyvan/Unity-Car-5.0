514cc4252c3baf3df28f79e05f5b47ea0759a5a6
Fan Chou <Chou.Fan@igt.com>
2015-11-03 14:26:38 +0800

    11.2.2 add car blob shadows. can not be seen from camera till now.

---
+++ Assets/_Scripts/BlobProjector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobProjector : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.parent.position + Vector3.up*8.25f;
+		transform.rotation = Quaternion.LookRotation (-Vector3.up, transform.parent.forward);
+	}
+}
